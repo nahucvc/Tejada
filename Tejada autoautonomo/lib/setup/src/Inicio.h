bae7a0ebb38ff2d98b362c476fc39ee301b28d95
Marcelo <Nahucvc@gmail.com>
2025-09-03 15:01:46 -0300

    se paso a arduino v2

---
+++ Tejada autoautonomo/lib/setup/src/Inicio.h
@@ -0,0 +1,14 @@
+#ifndef INICIO_H
+#define INICIO_H
+#include <Arduino.h>
+#include "adc.h"
+#include "dma.h"
+#include "tim.h"
+#include "stm32g4xx_it.h"
+#include "gpio.h"
+void iniciarPerifericos();
+
+
+
+
+#endif
---
+++ Tejada autoautonomo/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Inicio.h>
+
+uint32_t ADC_Valor=0;
+void setup() {
+  Serial.begin(115200);
+  iniciarPerifericos();
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
