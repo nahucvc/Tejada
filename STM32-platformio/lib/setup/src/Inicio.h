f7f9e7f5f576345e8fb78a682a9ccb1aab5e2189
Marcelo <Nahucvc@gmail.com>
2025-09-15 14:45:25 -0300

    Mejora del filtro

--- STM32-platformio/lib/setup/src/Inicio.h
+++ STM32-platformio/lib/setup/src/Inicio.h
@@ -29,7 +29,7 @@ private:
     float R; // Varianza de la medición
     float K; // Ganancia de Kalman
     LPF1 filt;
-    const float Fs = 1000.0f; // 1000 Hz -> T = 1 ms
+    const float Fs = 5000.0f; // 1000 Hz -> T = 1 ms
     const float fc = 10.0f;   // corta  (pasa continua y bajas)
 
 public:
